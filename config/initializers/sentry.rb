28d2a25832ad6d0c5400938d170a27be9f35dd31
cchoi94 <choicodes@gmail.com>
2022-02-14 01:54:38 -0500

    fixed sentry pipe var issue

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,7 +5,7 @@ Sentry.init do |config|
   # To activate performance monitoring, set one of these options.
   # We recommend adjusting the value in production:
   # set a uniform sample rate between 0.0 and 1.0
-  config.traces_sample_rate = 0.2 if Rails.pipe_env.production?
+  config.traces_sample_rate = 0.5
 
   # seperate environments into dashboard
   config.environments = %w[ production ]
