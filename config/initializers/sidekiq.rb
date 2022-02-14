a36e5b7d6c50b274da3294e01d8428b26d26ca3d
cchoi94 <choicodes@gmail.com>
2022-02-14 02:11:40 -0500

    secured sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,17 @@
+require 'sidekiq'
+require 'sidekiq/web'
+
 schedule_file = "config/schedule.yml"
 if File.exist?(schedule_file) && Sidekiq.server?
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
+
+Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+  # Protect against timing attacks:
+  # - See https://codahale.com/a-lesson-in-timing-attacks/
+  # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+  # - Use & (do not use &&) so that it doesn't short circuit.
+  # - Use digests to stop length information leaking
+  Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(user), ::Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq, :username))) &
+  Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq, :password)))
 end
