fd88afb794ecc4c536d539b15235ba2245909ae2
cchoi94 <choicodes@gmail.com>
2022-03-12 11:16:51 -0800

    added extra check to not save nil values

--- app/services/questrade_api/sync.rb
+++ app/services/questrade_api/sync.rb
@@ -18,11 +18,7 @@ module QuestradeApi
 
     def self.headers(integration)
       res = HTTParty.get("https://login.questrade.com/oauth2/token?grant_type=refresh_token&refresh_token=#{integration.decrypt_string(integration.refresh_token)}")
-      if res.success?
-        puts "@@@ Success"
-        puts res["access_token"]
-        puts res["refresh_token"]
-        puts res["api_server"]
+      if res.success? && res["access_token"].present? && res["refresh_token"].present? && res["api_server"].present?
         integration.update(
           access_token: integration.encrypt_string(res["access_token"]),
           refresh_token: integration.encrypt_string(res["refresh_token"]),
@@ -33,6 +29,28 @@ module QuestradeApi
           Host: res["api_server"],
           Authorization: "Bearer #{res["access_token"]}"
         }
+      end
+      if res["access_token"].nil? || res["refresh_token"].nil? || res["api_server"].nil?
+        puts "Questrade credentials error"
+        response = [
+          {
+            type: 'access_token',
+            value: res["access_token"]
+          },
+          {
+            type: 'refresh_token',
+            value: res["refresh_token"]
+          },
+          {
+            type: 'api_server',
+            value: res["api_server"]
+          }
+        ]
+        response.each do |r|
+          if r.value.nil?
+            puts "#{r.type} is nil"
+          end
+        end
       end
     rescue => error
       Rails.logger.error(error.message)
