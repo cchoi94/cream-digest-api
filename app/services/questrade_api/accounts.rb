1778918c3730e11369103805b11be802716890c7
cchoi94 <choicodes@gmail.com>
2022-03-17 11:29:54 -0700

    readded conditions and an extra puts to check accounts for questrade

--- app/services/questrade_api/accounts.rb
+++ app/services/questrade_api/accounts.rb
@@ -2,7 +2,10 @@ module QuestradeApi
   class Accounts < QuestradeApi::Sync
     def self.get(integration)
       new_headers = headers(integration)
+      puts new_headers
       res = HTTParty.get("#{integration.host_server}v1/accounts", headers: new_headers)
+      put "@@@ accounts"
+      puts res["accounts"]
       res["accounts"]
     rescue => error
       Rails.logger.error(error.message)
